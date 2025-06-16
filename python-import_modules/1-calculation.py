63b345d9d6528bbad35716b29bdb2c96280bffbf
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 14:55:19 +0000

    h

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
-
-def main():
-    a = 10
-    b = 5
-
-    result1 = add(a, b)
-    print("Add: %d" % result1)
-
-    result2 = subtract(a, b)
-    print("Subtract: %d" % result2)
-
-    result3 = multiply(a, b)
-    print("Multiply: %d" % result3)
-
-    result4 = divide(a, b)
-    print("Divide: %d" % result4)
-
-if __name__ == "__main__":
-    main()
+print("Add: {}".format(result1))
+print("Subtract: {}".format(result2))
+print("Multiply: {}".format(result3))
+print("Divide: {}".format(result4))
