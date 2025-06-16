adb4b2e7a6e8c71f5d7ab2ebee932a393313660d
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 14:56:34 +0000

    h

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-print("Add: {}".format(result1))
-print("Subtract: {}".format(result2))
-print("Multiply: {}".format(result3))
-print("Divide: {}".format(result4))
+from calculator_1 import add, subtract, multiply, divide
+
+def main():
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    print("Add: {}".format(result1))
+    result2 = subtract(a, b)
+    print("Subtract: {}".format(result2))
+    result3 = multiply(a, b)
+    print("Multiply: {}".format(result3))
+    result4 = divide(a, b)
+    print("Divide: {}".format(result4))
+
+if __name__ == "__main__":
+    main()
