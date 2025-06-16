8286a9764b20dafd04d20aef09b6084c7e8dbfdf
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 15:04:37 +0000

    k

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv[1:]  # Exclude the script name
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    else:
+        print(f"{count} argument{'s' if count != 1 else ''}:")
+
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
+
