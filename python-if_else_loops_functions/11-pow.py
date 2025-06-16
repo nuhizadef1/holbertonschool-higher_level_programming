32e43210b6be83c887f8126676b8ca56ebbed8d5
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 14:31:22 +0000

    j

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # Handle the case of b == 0
     if b == 0:
         return 1
-
-    # Handle negative powers
     if b < 0:
         a = 1 / a
         b = -b
-
     result = 1
     for _ in range(b):
         result *= a
-
     return result
+
+# Example of printing the output formatted to 2 decimal places:
+print(f"{pow(0.1, 2):.2f}")
