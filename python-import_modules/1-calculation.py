9fed3d36d56ca95890e6ec961e78f4e74fa692a4
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 14:58:01 +0000

    h

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
+from calculator_1 import add, sub, mul, div
 
 def main():
     a = 10
     b = 5
-    result1 = add(a, b)
-    print("Add: {}".format(result1))
-    result2 = subtract(a, b)
-    print("Subtract: {}".format(result2))
-    result3 = multiply(a, b)
-    print("Multiply: {}".format(result3))
-    result4 = divide(a, b)
-    print("Divide: {}".format(result4))
+
+    result1 = add(a, b)       # actually subtracts
+    print("{} + {} = {}".format(a, b, result1))
+
+    result2 = sub(a, b)       # actually adds
+    print("{} - {} = {}".format(a, b, result2))
+
+    result3 = mul(a, b)       # actually divides
+    print("{} * {} = {}".format(a, b, result3))
+
+    result4 = div(a, b)       # actually multiplies
+    print("{} / {} = {}".format(a, b, result4))
 
 if __name__ == "__main__":
     main()
