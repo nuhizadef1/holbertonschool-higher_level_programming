ad2c7198039b3f0d91daff1c1113ec372e96d7fc
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-16 13:25:56 +0000

    k

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,8 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 # Assume number is already assigned by some code you don't touch
 import random
-
 number = random.randint(-100, 100)  # This line is given and should not be changed
-
 print(number, end=' ')  # Print the number followed by a space, no newline yet
-
 if number > 0:
     print("is positive")
 elif number == 0:
