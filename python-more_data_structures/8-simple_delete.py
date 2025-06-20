2b6bc1e0e7826cf6a5d3f00a769b36865728d9ee
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-20 16:35:52 +0000

    j

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictionary[key]}")
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
