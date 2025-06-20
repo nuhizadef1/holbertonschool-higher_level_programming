91ed61ba8799388f61e92ad86c5b29a50187068e
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-20 15:55:37 +0000

    j

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix by squaring each element of the input matrix
+    return [[element ** 2 for element in row] for row in matrix]
