1bbf58b38cc0868526e7c5cf7b7b501454700d45
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-20 14:51:36 +0000

    h

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(c)) for c in range(97, 123) if c not in (101, 113)), end="")
+
+print("".join("{}".format(chr(c)) for c in range(97, 123) if c != 101 and c != 113), end="")
