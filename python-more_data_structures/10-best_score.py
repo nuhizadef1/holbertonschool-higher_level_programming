caa7602b3062874300e136c005d462a72c7523ff
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-21 16:15:23 +0000

    j

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,12 +2,6 @@
 def best_score(a_dictionary):
     """
     Returns the key with the biggest integer value in the dictionary.
-
-    Args:
-        a_dictionary (dict): Dictionary with values as integers.
-
-    Returns:
-        str or None: Key with the highest value, or None if dictionary is empty or None.
     """
     if not a_dictionary:
         return None
