2a9a635480ac808b49acfa1376c69d404455decf
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-21 16:05:34 +0000

    h

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """
+    Updates or adds a key-value pair in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to update.
+        key (str): The key to update or add.
+        value: The value to assign to the key.
+
+    Returns:
+        dict: The updated dictionary.
+    """
     a_dictionary[key] = value
+    return a_dictionary
