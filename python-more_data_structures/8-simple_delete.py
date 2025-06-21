b35bd24330c0dd10929845e1e3fc330ca0760298
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-21 16:08:12 +0000

    j

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key from a dictionary if it exists.
+
+    Args:
+        a_dictionary (dict): The dictionary to update.
+        key (str): The key to delete.
+
+    Returns:
+        dict: The updated dictionary.
+    """
     if key in a_dictionary:
         del a_dictionary[key]
+    return a_dictionary
