ea657ca1411f50582b816e5d77ce197c17bc4b36
nuhizadef1 <fatimenuhi15@gmail.com>
2025-06-21 16:38:53 +0000

    j

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             a = my_list_1[i]
             b = my_list_2[i]
-            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            if not isinstance(a, (int, float)) or \
+               not isinstance(b, (int, float)):
                 print("wrong type")
                 div = 0
             else:
