422ca00b71e86cac742710dbf8c52f075f9fd520
nuhizadef1 <fatimenuhi15@gmail.com>
2025-07-03 14:53:15 +0000

    j

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import pickle
 
 
 class CustomObject:
-    """This task is bad"""
+    """This class is about custom"""
 
     def __init__(self, name, age, is_student):
         self.name = name
@@ -16,40 +16,19 @@ class CustomObject:
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Serializes the object and saves it to a file."""
         try:
-            with open(filename, 'wb') as file:
-                pickle.dump(self, file)
-            print(f"Object serialized to {filename}")
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+
         except Exception as e:
-            print(f"Error during serialization: {e}")
+            print(f"Serialization Error: {e}")
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserializes the object from a file and returns an instance."""
         try:
-            with open(filename, 'rb') as file:
-                obj = pickle.load(file)
-                return obj
-            return True 
-
-        except FileNotFoundError:
-            return False 
-        except Exception:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj
+        except Exception as e:
+            print(f"Desrialization Error: {e}")
             return None
-
-
-if __name__ == "__main__":
-    # Create an instance of CustomObject
-    obj = CustomObject(name="John", age=25, is_student=True)
-    print("Original Object:")
-    obj.display()
-
-    # Serialize the object
-    obj.serialize("object.pkl")
-
-    # Deserialize the object into a new instance
-    new_obj = CustomObject.deserialize("object.pkl")
-    if new_obj:
-        print("\nDeserialized Object:")
-        new_obj.display()
