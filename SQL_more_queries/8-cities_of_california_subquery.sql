3102685a86f74d27f9421e65c7a1e657e07c17bb
nuhizadef1 <fatimenuhi15@gmail.com>
2025-07-10 17:01:17 +0000

    task

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- cities of california
-SELECT cities.id, states.state_id, cities.name
+SELECT id, name
 FROM cities
 WHERE name = (SELECT name FROM states WHERE name="California")
-ORDER BY cities.id ASC;
+ORDER BY id ASC;
