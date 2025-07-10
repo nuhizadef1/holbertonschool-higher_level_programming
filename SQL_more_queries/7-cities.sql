4030aa5cb4d5a5feb119a8a42ffc9b0f77e645ab
nuhizadef1 <fatimenuhi15@gmail.com>
2025-07-10 16:42:16 +0000

    task

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ USA hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities ( 
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL 
-	name VARCHAR(256) NOT NULL
+	state_id INT NOT NULL, 
+	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
